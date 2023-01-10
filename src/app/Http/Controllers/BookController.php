d74d45b64aa91b314d962bf394b2a6d55695718e
AlexZhernov <s20zernalek@venta.lv>
2023-01-10 16:21:09 +0200

    Full crud for book, image support, save before refactoring

--- src/app/Http/Controllers/BookController.php
+++ src/app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use Illuminate\Http\Request;
 use App\Models\Book;
 
@@ -50,8 +51,70 @@ class BookController extends Controller
         $book->price = $validatedData['price'];
         $book->year = $validatedData['year'];
         $book->display = (bool) ($validatedData['display'] ?? false);
+        if ($request->hasFile('image')) {
+            $uploadedFile = $request->file('image');
+            $extension = $uploadedFile->clientExtension();
+            $name = uniqid();
+            $book->image = $uploadedFile->storePubliclyAs(
+            '/',
+            $name . '.' . $extension,
+            'uploads'
+            );
+           }
+           
         $book->save();
         return redirect('/books');
     }
+
+    public function update(Book $book)
+    {
+        $authors = Author::orderBy('name', 'asc')->get();
+        return view(
+            'book.form',
+            [
+                'title' => 'Rediģēt grāmatu',
+                'book' => $book,
+                'authors' => $authors,
+            ]
+        );
+    }
+    public function patch(Book $book, Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|min:3|max:256',
+            'author_id' => 'required',
+            'description' => 'nullable',
+            'price' => 'nullable|numeric',
+            'year' => 'numeric',
+            'image' => 'nullable|image',
+            'display' => 'nullable'
+        ]);
+        $book->name = $validatedData['name'];
+        $book->author_id = $validatedData['author_id'];
+        $book->description = $validatedData['description'];
+        $book->price = $validatedData['price'];
+        $book->year = $validatedData['year'];
+        $book->display = (bool) ($validatedData['display'] ?? false);
+        if ($request->hasFile('image')) {
+            $uploadedFile = $request->file('image');
+            $extension = $uploadedFile->clientExtension();
+            $name = uniqid();
+            $book->image = $uploadedFile->storePubliclyAs(
+            '/',
+            $name . '.' . $extension,
+            'uploads'
+            );
+           }
+           
+        $book->save();
+        return redirect('/books/update/' . $book->id);
+    }
+
+    public function delete(Book $book)
+    {
+        $book->delete();
+        return redirect('/books');
+    }
+
 
 }
